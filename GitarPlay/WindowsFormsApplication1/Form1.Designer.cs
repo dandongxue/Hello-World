09dab48fa302c59817338094d806b396833e7d10
unknown <240861544@qq.com>
2016-10-05 22:21:49 +0800

    first day

--- GitarPlay/WindowsFormsApplication1/Form1.Designer.cs
+++ GitarPlay/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,24 +29,127 @@
         private void InitializeComponent()
         {
             this.trviewGitar = new System.Windows.Forms.TreeView();
+            this.picboxMain = new System.Windows.Forms.PictureBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.tboxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxMain)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // trviewGitar
             // 
-            this.trviewGitar.Location = new System.Drawing.Point(5, 14);
+            this.trviewGitar.Location = new System.Drawing.Point(0, 72);
+            this.trviewGitar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.trviewGitar.Name = "trviewGitar";
-            this.trviewGitar.Size = new System.Drawing.Size(121, 392);
+            this.trviewGitar.Size = new System.Drawing.Size(184, 572);
             this.trviewGitar.TabIndex = 0;
+            // 
+            // picboxMain
+            // 
+            this.picboxMain.Location = new System.Drawing.Point(273, 18);
+            this.picboxMain.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.picboxMain.Name = "picboxMain";
+            this.picboxMain.Size = new System.Drawing.Size(492, 644);
+            this.picboxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxMain.TabIndex = 1;
+            this.picboxMain.TabStop = false;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(108, 36);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(69, 29);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "测试";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnTest);
+            this.panel1.Controls.Add(this.tboxSearch);
+            this.panel1.Controls.Add(this.lb1);
+            this.panel1.Controls.Add(this.trviewGitar);
+            this.panel1.Location = new System.Drawing.Point(6, 18);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(184, 644);
+            this.panel1.TabIndex = 3;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(2, 8);
+            this.lb1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(80, 17);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "吉他谱搜索：";
+            // 
+            // tboxSearch
+            // 
+            this.tboxSearch.Location = new System.Drawing.Point(78, 5);
+            this.tboxSearch.Name = "tboxSearch";
+            this.tboxSearch.Size = new System.Drawing.Size(100, 23);
+            this.tboxSearch.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(19, 36);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(71, 29);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "搜索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 277);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 72);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "上一页";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(766, 277);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 72);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "下一页";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 410);
-            this.Controls.Add(this.trviewGitar);
+            this.ClientSize = new System.Drawing.Size(841, 672);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picboxMain);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "吉他show";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxMain)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +157,14 @@
         #endregion
 
         private System.Windows.Forms.TreeView trviewGitar;
+        private System.Windows.Forms.PictureBox picboxMain;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tboxSearch;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- GitarPlay/WindowsFormsApplication1/Form1.cs
+++ GitarPlay/WindowsFormsApplication1/Form1.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private PublicClass.AutoSizeForm autoform = new PublicClass.AutoSizeForm();
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +28,31 @@ namespace WindowsFormsApplication1
                 TreeNode NodePic = new TreeNode("Pic"+i.ToString());
                 node.Nodes.Add(NodePic);
             }
+            this.MouseWheel += Form1_MouseWheel;
+        }
+        void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0) //放大图片
+            {
+                picboxMain.Size = new Size(picboxMain.Width + 50, picboxMain.Height + 50);
+            }
+            else
+            {  //缩小图片
+                picboxMain.Size = new Size(picboxMain.Width - 50, picboxMain.Height - 50);
+            }
+            //设置图片在窗体居中
+            picboxMain.Location = new Point(( picboxMain.Width) / 2, (picboxMain.Height) / 2);
+            //picboxMain.Location = new Point((this.Width - picboxMain.Width) / 2, (this.Height - picboxMain.Height) / 2);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            picboxMain.Image = Image.FromFile("E:\\Bayes\\GitResp\\Gitar-Play\\GitarPlay\\WindowsFormsApplication1\\1.jpg");
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            autoform.controlAutoSize(this);
         }
     }
 }
